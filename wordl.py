980442616a29692d41b03c9de47e783764634311
xing216 <2018mjb@gmail.com>
2022-03-21 18:44:24 -0400

    Fixed character showing as incorrect to be correct

--- wordl.py
+++ wordl.py
@@ -1,19 +1,16 @@
 import json
 import random
-
 class bcolors:
     BLUE = '\033[94m'
     GREEN = '\033[92m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
-
 word = "three"
 # with open("words.json", "r") as f:
 #     words = json.load(f)
 #     index = random.randint(0, 10663)
 #     word = words[index]
-
-tries = 0
+tries = 1
 tries_limit = 5
 letter_count = {}
 for h in word:
@@ -21,8 +18,8 @@ for h in word:
         letter_count[h] = 1
     else:
         letter_count[h] += 1
-while tries < tries_limit:
-    guess = input(f"Try {tries+1}: ")
+while tries <= tries_limit:
+    guess = input(f"Try {tries}: ")
     num_of_correct = 0
     correct = []
     misplaced = []
@@ -35,11 +32,11 @@ while tries < tries_limit:
         else:
             letter_used_count[k] += 1
     for i,j in enumerate(guess):
-        if j == word[i] and letter_count[j] <= letter_used_count[j]:
+        if j == word[i] and letter_count[j] >= letter_used_count[j] > 0:
             correct.append(j)
             result.append(f"{bcolors.GREEN}{j}{bcolors.ENDC}")
             num_of_correct +=1
-        elif j in word and j not in misplaced and j not in correct and letter_count[j] <= letter_used_count[j]:
+        elif j in word and j not in misplaced and j not in correct and letter_count[j] >= letter_used_count[j] > 0:
             misplaced.append(j)
             result.append(f"{bcolors.BLUE}{j}{bcolors.ENDC}")
         elif j not in incorrect:
@@ -48,6 +45,8 @@ while tries < tries_limit:
     if num_of_correct == 5:
         print(f"Yay! You got all 5 letters: {word}")
         break
+    elif tries == tries_limit:
+        print("Correct word: " + word)
     else:
         print(f"{''.join(result)}")
     tries +=1
